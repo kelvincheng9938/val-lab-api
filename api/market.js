eed6f0cf6e19dd7cdc8e04d13bff6d0fc0351442
kelvincheng9938 <kelvincheng9938@gmail.com>
2025-08-25 18:05:57 +0800

    Update market.js

--- api/market.js
+++ api/market.js
@@ -1,3 +1,8 @@
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+if (req.method === 'OPTIONS') return res.status(200).end();
+
 module.exports = async (req, res) => {
   // Demo：前端主要要格式正確；真值靠上游 key
   const out = {
